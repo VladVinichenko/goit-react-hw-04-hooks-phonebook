3ea9e4e2fcc0dd6a7710f0319569f5af325a8f4c
VladVinichenko <vinnyxxxl925@gmail.com>
2022-01-27 15:27:34 +0200

    complete

--- src/App.js
+++ src/App.js
@@ -32,8 +32,7 @@ function App() {
 
   const onDeleteContact = (removeId) => {
     setContacts(contacts.filter(el => el.id !== removeId))
-    setFilter(contacts.filter(el => el.id !== removeId))
-    onFilterChange(filterInput)
+    filterInput.length > 0 && setFilter(filter.filter(el => el.id !== removeId))
   }
 
   const onInput = (evt) => {
@@ -42,7 +41,7 @@ function App() {
   }
 
   const onInputFilter = (evt) => {
-    setFilterInput(evt.target.value)
+    setFilterInput(evt.target.value.trim())
     onFilterChange(evt.target.value)
   }
 
@@ -68,79 +67,6 @@ function App() {
   )
 
 }
-
-
-// class App extends Component {
-//   state = {
-//     contacts: [],
-//     filter: '',
-//     name: '',
-//     number: '',
-//     filterInput: ''
-//   }
-
-
-
-//   onAddContact = (evt) => {
-//     evt.preventDefault()
-//     if (this.state.name.trim().length > 0) {
-//       this.setState(prevState => {
-//         const contacts = [...prevState.contacts, { name: this.state.name, number: this.state.number, id: nanoid() }]
-//         return { contacts: contacts }
-//       })
-//     }
-//   }
-
-//   onDeleteContact = (removeId) => {
-//     this.setState({ contacts: this.state.contacts.filter(el => el.id !== removeId) })
-//     this.state.filterInput && this.setState({ filter: this.state.contacts.filter(el => el.id !== removeId) })
-//     this.onFilterChange()
-//   }
-
-//   onInput = (evt) => {
-//     const data = {}
-//     data[evt.target.name] = evt.target.value
-//     this.setState(data)
-//   }
-
-//   onFilterChange = () => {
-//     this.setState(prevState => {
-//       if (prevState.filterInput.trim().length > 0) {
-//         return { filter: prevState.contacts.filter(el => el.name.toLowerCase().includes(prevState.filterInput.toLowerCase())) }
-//       }
-//       return { filter: '' }
-//     })
-//   }
-
-//   onInputFilter = (evt) => {
-//     this.setState({ filterInput: evt.target.value })
-//     this.onFilterChange()
-//   }
-
-//   componentDidMount() {
-//     const contacts = JSON.parse(localStorage.getItem('contacts'))
-//     contacts && this.setState({ contacts: contacts })
-//   }
-
-//   componentDidUpdate(prevProps, prevState) {
-//     this.state.contacts !== prevProps.contacts && localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-//   }
-
-//   render() {
-//     const renderList = this.state.filterInput.length > 0 ? this.state.filter : this.state.contacts
-//     return (
-//       <Fragment>
-//         <Section>
-//           <h1 className={s.title}>Phonebook</h1>
-//           <ContactForm onAddContact={this.onAddContact} onInputName={this.onInput} onInputTel={this.onInput} />
-//           <h2 className={s.title}>Contacts</h2>
-//           <Filter onInputFilter={this.onInputFilter} />
-//           <ContactList renderList={renderList} onDeleteContact={this.onDeleteContact} />
-//         </Section>
-//       </Fragment>
-//     )
-//   }
-// }
 
 
 export default App
